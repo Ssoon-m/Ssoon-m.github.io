24649832dfcacb01365b5909f463fc5fbc294fe8
순민 <tkksm2@gmail.com>
2022-06-26 14:42:27 +0900

    Feat: 헤더 레이아웃 구성

---
+++ src/components/Main/ProfileImage.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import styled from "@emotion/styled"
+
+const PROFILE_IMAGE_LINK =
+  "https://thumbnail7.coupangcdn.com/thumbnails/remote/292x292ex/image/rs_quotation_api/dbbmlmae/a99b2ae4c2a34dad95980bb7c29d54e0.jpg"
+
+const ProfileImageWrapper = styled.img`
+  width: 120px;
+  height: 120px;
+  margin-bottom: 30px;
+  border-radius: 50%;
+  object-fit: cover;
+`
+
+const ProfileImage: React.FC = function () {
+  return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />
+}
+
+export default ProfileImage
