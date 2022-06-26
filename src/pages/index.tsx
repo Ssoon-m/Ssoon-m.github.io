95f014c6578a74126999d88c7978ed4eafb76faf
순민 <tkksm2@gmail.com>
2022-06-26 14:45:55 +0900

    Feat: footer 레이아웃 구성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,12 @@ import React from "react"
 import styled from "@emotion/styled"
 import GlobalStyle from "../components/Common/GlobalStyle"
 import Introduction from "../components/Main/Introduction"
+import Footer from "../components/Common/Footer"
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100%;
+  height: 100vh;
 `
 
 const IndexPage: React.FC = function () {
@@ -14,6 +15,7 @@ const IndexPage: React.FC = function () {
     <Container>
       <GlobalStyle />
       <Introduction />
+      <Footer />
     </Container>
   )
 }
